40158c9e91b11524672b8f8b1e67a3d8bdf45cd7
Massimo <mambrosi98@gmail.com>
2025-01-13 10:04:58 +0100

    add error handling on getPokemon requests

--- src/main/java/com/hangovers/funpokedex/Utils.java
+++ src/main/java/com/hangovers/funpokedex/Utils.java
@@ -1,6 +1,8 @@
 package com.hangovers.funpokedex;
 
 import com.hangovers.funpokedex.model.Pokemon;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
+
 
 public class Utils {
 
@@ -18,10 +20,23 @@ public class Utils {
     }
 
     /**
-     * Used to gracefully handle pokeapi errors
+     * Used to gracefully handle user requests with non-existing pokemons
+     * or pokeapi 404 errors
+     *
      * @return missingno data
      */
     public static Pokemon missingno() {
         return new Pokemon("MissingNo.", "The Pokémon you are looking for does not exist.", "Unknown", false);
+    }
+
+    /**
+     * Used to gracefully handle other pokeapi errors
+     * @return badEgg data
+     */
+    public static Pokemon badEgg(HttpClientResponseException e) {
+        return new Pokemon("Bad EGG",
+                e.getMessage(),
+                "Unknown",
+                false);
     }
 }
--- src/main/java/com/hangovers/funpokedex/service/impl/FunpokedexServiceImplementation.java
+++ src/main/java/com/hangovers/funpokedex/service/impl/FunpokedexServiceImplementation.java
@@ -4,6 +4,7 @@ import com.hangovers.funpokedex.clients.pokeapi.PokeapiClient;
 import com.hangovers.funpokedex.clients.pokeapi.models.PokeApiResponse;
 import com.hangovers.funpokedex.model.Pokemon;
 import com.hangovers.funpokedex.service.FunpokedexService;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import jakarta.inject.Singleton;
 import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
@@ -28,6 +29,28 @@ public class FunpokedexServiceImplementation implements FunpokedexService {
      */
     public Publisher<Pokemon> getPokemon(String name) {
         log.info("Fetching data from pokeapi or cache for pokemon name {}", name);
-        return Mono.from(pokeapiClient.fetchPokemonSpecies(name)).map(PokeApiResponse::asPokemon);
+        return Mono.from(pokeapiClient.fetchPokemonSpecies(name))
+                .map(PokeApiResponse::asPokemon)
+                .onErrorResume(this::handleError);
+    }
+
+    private Mono<Pokemon> handleError(Throwable t) {
+        switch(t) {
+            case HttpClientResponseException e
+                    when e.getStatus() == NOT_FOUND -> {
+                log.error("Pokèmon not found");
+                return Mono.just(Utils.missingno());
+            }
+
+            case HttpClientResponseException e -> {
+                log.error("Error while retrieving pokemon. {}", e.getMessage());
+                return Mono.just(Utils.badEgg(e));
+            }
+
+            default -> {
+                log.error("Unexpected error while retrieving pokemon. {}", t.getMessage());
+                return Mono.error(t);
+            }
+        }
     }
 }
